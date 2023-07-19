ca7ac475a0b6cbdb0163017194e7d0d68c31f4e4
DAMIENDURAND64 <damien64durand@gmail.com>
2023-07-19 22:09:01 +0200

    work in progress

--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -1,5 +1,8 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, HttpException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcryptjs';
+import * as jwt from 'jsonwebtoken';
+import { UserType } from '@prisma/client';
 
 interface SignupParams {
   name: string;
@@ -8,15 +11,57 @@ interface SignupParams {
   phone: string;
 }
 
+interface SigninParams {
+  email: string;
+  password: string;
+}
+
 @Injectable()
 export class AuthService {
   constructor(private readonly prismaService: PrismaService) {}
-  async signup({ email }: SignupParams) {
+
+  private async generateJWT(name: string, id: number) {
+    return jwt.sign({ name, id }, process.env.JWT_SECRET, {
+      expiresIn: 360000,
+    });
+  }
+
+  async signup({ email, password, phone, name }: SignupParams) {
     const userExists = await this.prismaService.user.findUnique({
       where: { email },
     });
     if (userExists) {
       throw new ConflictException('Email already exists');
     }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await this.prismaService.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+        phone,
+        name,
+        user_type: UserType.BUYER,
+      },
+    });
+
+    return this.generateJWT(user.name, user.id);
+  }
+
+  async signin({ email, password }: SigninParams) {
+    const user = await this.prismaService.user.findUnique({
+      where: { email },
+    });
+    if (!user) {
+      throw new HttpException('No user found', 400);
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      throw new HttpException('Invalid credentials', 400);
+    }
+
+    return this.generateJWT(user.name, user.id);
   }
 }
