1080ac69d36b56fd5bccce9462758671f501c801
DAMIENDURAND64 <damien64durand@gmail.com>
2023-07-20 19:08:20 +0200

    working on different type of user

--- src/user/auth/auth.controller.ts
+++ src/user/auth/auth.controller.ts
@@ -1,12 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Param, ParseEnumPipe, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignupDto, SigninDto } from '../dtos/dto';
+import { UserType } from '@prisma/client';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-  @Post('/signup')
-  signup(@Body() body: SignupDto) {
+  @Post('/signup/:userType')
+  signup(
+    @Body() body: SignupDto,
+    @Param('userType', new ParseEnumPipe(UserType)) userType: UserType,
+  ) {
     return this.authService.signup(body);
   }
 
